1473859a820e038f2512935689924ae94461a326
Marijn Haverbeke <marijnh@gmail.com>
2015-02-18 16:01:54 +0100

    Delay feature detection of tooltips to plugin_loaded

--- tern.py
+++ tern.py
@@ -13,8 +13,6 @@ def is_js_file(view):
 files = {}
 arghints_enabled = False
 arghints_type = "status"
-if "show_popup" in dir(sublime.View):
-  arghints_type = "tooltip"
 tern_command = None
 tern_arguments = []
 tern_arghint = ""
@@ -548,6 +546,8 @@ def plugin_loaded():
   settings = sublime.load_settings("Preferences.sublime-settings")
   arghints_enabled = settings.get("tern_argument_hints", False)
   if arghints_enabled:
+    if "show_popup" in dir(sublime.View):
+      arghints_type = "tooltip"
     arghints_type = settings.get("tern_argument_hints_type", arghints_type)
   tern_arguments = settings.get("tern_arguments", [])
   tern_command = settings.get("tern_command", None)
